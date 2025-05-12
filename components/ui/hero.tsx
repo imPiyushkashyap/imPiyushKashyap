4fe26c884684d50d44649ab7e73a3ad82e8a1f2c
Piyush Kashyap <piyush8580kashyap@gmail.com>
2025-05-12 19:10:23 +0530

    fix: minor bug

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { TextGenerateEffect } from "./textGeneration";
 const words =
   "I build performant web apps With expertise in Next.js, TypeScript, Payload CMS, Vercel AI SDK and AI-driven features.";
